29e68f270086b42de1faeeed6cf2383604f2376e
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-03 10:34:38 -0700

    Modified StudentServiceImpl and BookServiceImpl service classes

--- src/main/java/org/example/service/BookServiceImpl.java
+++ src/main/java/org/example/service/BookServiceImpl.java
@@ -1,15 +1,12 @@
 package org.example.service;
 
 import org.example.entity.Book;
-import org.example.entity.TransactionHistory;
+import org.example.entity.Student;
 import org.example.repository.BookRepository;
 import org.example.repository.StudentRepository;
 import org.example.repository.TransactionHistoryRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 /**
@@ -21,16 +18,12 @@ public class BookServiceImpl implements BookService {
 
     @Autowired
     private BookRepository bookRepository;
-
     @Autowired
-    private StudentRepository studentRepository;
+    private StudentService studentService;
 
     @Autowired
     private TransactionHistoryRepository transactionHistoryRepository;
 
-
-        private final Logger LOGGER =
-            LoggerFactory.getLogger(BookService.class);
     @Override
     public List<Book> fetchBookList() {
         return bookRepository.findAll();
@@ -38,36 +31,62 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<Book> searchBooksByTitle(String title) {
+
         return bookRepository.findByTitleContaining(title);
     }
 
     @Override
-    public String borrowBookDetails(String title) {
+    public String borrowBookDetails(String title,int student_id) {
         Book book = bookRepository.getBookDetails(title);
         if (book == null) {
                return "Book not found or not available.Please try typing correct name of book or contact admin to check book availability";
         }
-        StringBuilder result = new StringBuilder();
-        result.append("Book borrowed \n").append("Name: ").append(book.getTitle()).append("\n").append("Author: ").append(book.getAuthor());
+        String result = "Book borrowed \n" + "Name: " + book.getTitle() + "\n" + "Author: " + book.getAuthor();
         bookRepository.updateTable(title);
-        transactionHistoryRepository.updateTransactionHistory(book.getId(), book.getTitle(),"borrowed");
-        return result.toString();
+        Student student = studentService.getStudentById(student_id);
+        transactionHistoryRepository.updateTransactionHistory(book.getId(),student_id, "borrowed",book.getTitle(),student.getEmailId());
+        return result;
     }
 
     @Override
-    public String returnBookDetails(String title) {
+    public String returnBookDetails(String title, int student_id) {
         Book book = bookRepository.getBookDetails(title);
         StringBuilder result = new StringBuilder();
-        //result.append("Return book \n").append("Name: ").append(book.getTitle()).append("\n").append("Author: ").append(book.getAuthor());
         bookRepository.modifyTable(title);
-        transactionHistoryRepository.updateTransactionHistory(book.getId(),book.getTitle(), "returned");
+        Student student = studentService.getStudentById(student_id);
+        transactionHistoryRepository.updateTransactionHistory(book.getId(),student_id,"returned",book.getTitle(),student.getEmailId());
         return "Book returned";
     }
 
     @Override
-    public List<TransactionHistory> showTransactionHistory(String title) {
-       // Book book = bookRepository.getBookDetails(title);
-        return transactionHistoryRepository.getTransactionHistory(title);
+    public String addBookDetails(String title,String bookAuthor,int quantity) {
+        List<Book> books= fetchBookList();
+        for(Book b:books) {
+            if(b.getTitle().equals(title)){
+                return "Book already exists";
+            }
+        }
+        bookRepository.addToTable(title, bookAuthor, quantity);
+        return "Book added to table";
+    }
+
+    @Override
+    public String getBookById(int bookID) {
+        Book b = bookRepository.findById(bookID);
+        bookRepository.updateTable(b.getTitle());
+        return "Book updated";
+    }
+
+    @Override
+    public String deleteById(int bookID) {
+        StringBuilder result = new StringBuilder();
+        Book book = bookRepository.findById(bookID);
+        transactionHistoryRepository.deleteBookById(bookID);
+        bookRepository.deleteBookFromTable(bookID);
+        if(book.getQuantity_available() <=0){
+            return "No book available to delete";
+        }
+        return "Book successfully deleted";
     }
 
 }
--- src/main/java/org/example/service/StudentServiceImpl.java
+++ src/main/java/org/example/service/StudentServiceImpl.java
@@ -5,7 +5,12 @@ import org.example.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,6 +27,60 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public List<Student> fetchStudentList() {
         return studentRepository.findAll();
+    }
+
+    @Override
+    public Student getStudentById(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void saveStudents(String name,String emailid) {
+        studentRepository.saveStudent(name,emailid);
+    }
+
+    @Override
+    public void updateStudent(int id,String name,String emailid) {
+        studentRepository.updateStudent(id,name,emailid);
+    }
+
+    @Override
+    public void deleteStudent(Integer id) {
+        studentRepository.deleteStudentByID(id);
+    }
+
+    public void registerStudent(String name,String emailId,String password,String role) {
+    studentRepository.registerStudent(name,emailId,password,role);
+    }
+//    @Override
+//    public UserDetails loadUserByUsername(String emailid) throws UsernameNotFoundException {
+//        Student student = studentRepository.findByUsername(emailid);
+//        if (student == null) {
+//            throw new UsernameNotFoundException("User not found");
+//        }
+//        return new org.springframework.security.core.userdetails.User(student.getEmailId(), student.getPassword(), new ArrayList<>());
+//    }
+
+    @Override
+    public boolean isValidUser(String emailid, String password) {
+        Student user = studentRepository.findByUsername(emailid);
+
+        if (user != null && user.getPassword().equals(password)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isStudent(String emailid, String password) {
+        Student user = studentRepository.findUserRole(emailid);
+
+        if (user != null && user.getRole().equals("student")) {
+            return true;
+        }
+
+        return false;
     }
 
 }
