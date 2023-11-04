8b4ea7a3220da28b49ef2b9bda06365797a80cc4
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-03 18:25:31 -0700

    Modified TransactionHistoryRepository class to write new query methods

--- src/main/java/org/example/repository/TransactionHistoryRepository.java
+++ src/main/java/org/example/repository/TransactionHistoryRepository.java
@@ -6,23 +6,29 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Integer> {
     @Modifying
     @Transactional
-    @Query(value = "insert into transaction_history (book_id,student_id,action,title,emailid) values (?1,?2,?3,?4,?5)" ,nativeQuery = true)
-    void  updateTransactionHistory(int bookId,int student_id,String action, String title,String emailid);
+    @Query(value = "insert into transaction_history (book_id,student_id,action,title,emailid,due_date) values (?1,?2,?3,?4,?5,?6)" ,nativeQuery = true)
+    void  updateTransactionHistory(int bookId, int student_id, String action, String title, String emailid, LocalDate due_date);
 
-//    @Query(value = "select * from transaction_history",nativeQuery = true)
-//    List<TransactionHistory> findStudentById();
-//
-    @Query(value = "select * from transaction_history where student_id=?1",nativeQuery = true)
-    List<TransactionHistory> getTransactionHistory(int student_id);
+    @Query(value = "select * from transaction_history where emailid=?1",nativeQuery = true)
+    List<TransactionHistory> getTransactionHistory(String emailid);
 
     @Modifying
     @Transactional
     @Query(value = "Delete from transaction_history where book_id=?1",nativeQuery = true)
     void deleteBookById(int bookID);
+
+    @Query(value = "select * from transaction_history where emailid=?1 and title=?2 and action=?3",nativeQuery = true)
+    List<TransactionHistory> getBorrowHistory(String emailid, String title,String action);
+
+    @Query(value = "select * from transaction_history where emailid=?1 and title=?2 and action=?3 limit 1",nativeQuery = true)
+    TransactionHistory getLastRowHistory(String emailid,String title,String action);
 }
